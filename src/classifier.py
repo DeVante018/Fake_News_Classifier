112ad126952828b8d19544be958a07fd5644ac10
DeVante <DeVantef@buffalo.edu>
2020-12-04 11:45:43 -0500

    main function to run the classifier

--- src/classifier.py
+++ src/classifier.py
@@ -1,7 +1,8 @@
 # this is the main file to run to test if something is fake news or not
-
+import image_classifier
+import text_classifier
 
 if __name__ == '__main__':
-    print("hello world\n")
-
-
+    text_predictor = text_classifier.train_text()
+    #image_predictor = image_classifier.train_images()
+    print(" process done...")
