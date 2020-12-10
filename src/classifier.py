44608a0e72bcca9b823676df1d5134e74496eb57
DeVante <DeVantef@buffalo.edu>
2020-12-10 12:19:54 -0500

    delete old classifier

--- src/classifier.py
+++
@@ -1,8 +0,0 @@
-# this is the main file to run to test if something is fake news or not
-import image_classifier
-import text_classifier
-
-if __name__ == '__main__':
-    text_predictor = text_classifier.train_text()
-    #image_predictor = image_classifier.train_images()
-    print(" process done...")
